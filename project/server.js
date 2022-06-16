b231494f76d6bd277499475aab02d6c2b9c4ef46
Darshanbhoyar1996 <bhoyardarshan5@gmail.com>
2022-06-16 10:14:14 +0530

    adding

---
+++ project/server.js
@@ -0,0 +1,15 @@
+// copied this code block from slack
+const PORT = 4001;
+const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use('/api', router);
+server.listen(PORT, () => console.log(`JSON Server is running on port ${PORT}`));
+
+// writing code from here
+
+ 
